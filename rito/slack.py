53fb79f98853a0f76f49a8c33da881de104c6fad
Nat Quayle Nelson <natquaylenelson@gmail.com>
2020-10-27 13:13:19 -0600

    add docs on slack app permissions

--- rito/slack.py
+++ rito/slack.py
@@ -4,7 +4,8 @@ import os
 
 if 'RITO_SLACK_TOKEN' not in os.environ:
     print("To use Rito's slack functions, first create a Slack app on your workspace following these instructions: https://api.slack.com/messaging/sending#getting_started")
-    print("After creating the app and requesting the right permissions, copy its auth token into an environment variable called RITO_SLACK_TOKEN")
+    print("Your app needs the permissions channel:read, chat:write, and chat:write.public")
+    print("After creating the app and installing it to your workspace, copy its auth token into an environment variable called RITO_SLACK_TOKEN")
     exit(1)
 
 auth_token = os.environ['RITO_SLACK_TOKEN']
